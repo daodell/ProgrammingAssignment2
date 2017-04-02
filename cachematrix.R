24b5aee1489c3816fa342e266664435282fef4f1
daodell <daodell@users.noreply.github.com>
2017-04-02 13:21:34 -0400

    getcache function added and working

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix() )  {
+
+        cache <- NULL
         
-        invcache <- NULL
-        
-        # setter function to set cache
-        set <- function(cache) {
-                x <<- cache
+        # setter function
+        set <- function(x) {
+                cache <<- x
         }
         
         # getter function
         get <- function() x
+        
+        # getter matrix inverse cache function
+        getcache <- function() cache
         #list
-        list (set = set, get=get)
+        list (set = set, get=get, getcache=getcache)
 }
 
 
@@ -25,6 +28,9 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         y <- x$get()     
         result <- solve(y)
+       
+        
+        x$set(result)
         result
          
                 
